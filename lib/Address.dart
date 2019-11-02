42a490e97921e8aea59079a125f636226e5cb9f1
Anna Tranquillini <annatranquillini@gmail.com>
2019-11-02 20:22:04 +0100

    Aggiunta oggetti con informazioni su indirizzo e compagnia dell'user

---
+++ lib/Address.dart
@@ -0,0 +1,36 @@
+class Address {
+  String street;
+  String suite;
+  String city;
+  String zipCode;
+
+  Address(
+      {
+        this.street,
+        this.suite,
+        this.city,
+        this.zipCode,
+      });
+
+  factory Address.fromJson(Map<String, dynamic> json) {
+    return Address(
+      street: json['street'] as String ?? '',
+      suite: json['suite'] as String?? '',
+      city: json['city'] as String?? '',
+      zipCode: json['zipcode'] as String?? '',
+
+
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'street': street,
+      'suite': suite ?? '',
+      'city': city,
+      'zipCode': zipCode,
+    };
+  }
+
+
+}
